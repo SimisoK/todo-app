c10a590f6e5f90d445f6488e69a4cd3f96e2cb21
Simiso K Mdluli <simisok22@gmail.com>
2025-04-01 00:10:26 +0200

    fix: remove unnecessary blank line in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Tabs } from "./components/Tabs"
 import { TodoInput } from "./components/TodoInput"
 import { TodoList } from "./components/TodoList"
 
+
 import { useState, useEffect } from 'react'
 
 function App() {
